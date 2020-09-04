2406db0824c88613ebb7fac1ca51f04810279fcb
Amar <mailamarm@gmail.com>
2020-09-04 18:20:41 +0530

    first commit

---
+++ src/main/java/abstraction/Interface1.java
@@ -0,0 +1,5 @@
+package abstraction;
+
+public interface Interface1 {
+    void show();
+}
---
+++ src/main/java/abstraction/Vehicle.java
@@ -0,0 +1,9 @@
+package abstraction;
+
+abstract class Vehicle
+{
+    abstract void start();
+    abstract void wheels();
+
+
+}
